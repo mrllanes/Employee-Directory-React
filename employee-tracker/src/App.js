11f54aacafaeeded22c9be594c2c3470d8185e43
Mr. Llanes <llanesm@gmail.com>
2021-04-20 19:10:17 -0500

    API file and components files created and populated.

--- employee-tracker/src/App.js
+++ employee-tracker/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Heading from "./components/Heading";
+import Table from "./components/Table";
 
 function App() {
     return (
         <div className="App">
             <Heading />
+            <Table />
         </div>
     );
 }
--- employee-tracker/src/components/Table.js
+++ employee-tracker/src/components/Table.js
@@ -1,26 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import API from "../utils/API";
 
 export default function Table() {
+    const [employees, setEmployees] = useState([]);
+    useEffect(() => {
+        // For demonstration purposes, we mock an API call.
+        API.getUsers().then((res) => {
+            console.log(res.data.results);
+            setEmployees(res.data.results);
+            console.log("Employees:");
+            console.log(employees);
+        });
+    }, []);
+
     return (
-        <table className="table table-striped table-dark">
-            <thead>
-                <tr>
-                    <th scope="col">Image</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">E-mail</th>
-                    <th scope="col">Phone</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                </tr>
-            </tbody>
-        </table>
+        <div className="container">
+            <table className="table table-striped table-dark">
+                <thead>
+                    <tr>
+                        <th scope="col">Image</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">E-mail</th>
+                        <th scope="col">Phone</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {employees.map((emp) => (
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            <td>@mdo</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     );
 }
